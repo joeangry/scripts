3f2e5222f664ca391734bcafe1ce5a341f97a0f4
Kammersgaard <4882368+joeangry@users.noreply.github.com>
2022-04-16 23:33:13 +0200

    Setup docker engine script

---
+++ bash/setup-docker.sh
@@ -0,0 +1,18 @@
+# Setup required packages
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# Docker GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Setup Docker source
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install docker engine
+sudo apt-get update && sudo apt-get -y install docker-ce docker-ce-cli containerd.io
